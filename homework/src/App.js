9436a575423477e8c30b317089e99038b7380c21
giang <iservice91@gmail.com>
2019-10-01 21:50:00 +0700

    tao nhanh page-home

---
+++ homework/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import HomePage from './pages/home/home';
+
+function App() {
+  return (
+    <React.Fragment>
+      <HomePage></HomePage>
+    </React.Fragment>
+  )
+}
+
+export default App;
---
+++ homework/src/pages/home/home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './home.scss';
+
+
+class HomePage extends React.Component{
+    render(){
+        return(
+            <React.Fragment>
+                dfghdjfgdfgj
+            </React.Fragment>
+        )
+    }
+}
+
+export default HomePage
