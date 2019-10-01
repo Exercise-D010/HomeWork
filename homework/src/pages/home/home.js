b8d7e821cb43fcadb97c48df604a57bcb38b384c
giang <iservice91@gmail.com>
2019-10-01 22:07:38 +0700

    tao nhanh login

--- homework/src/pages/home/home.js
+++ homework/src/pages/home/home.js
@@ -6,7 +6,7 @@ class HomePage extends React.Component{
     render(){
         return(
             <React.Fragment>
-                dfghdjfgdfgj
+                
             </React.Fragment>
         )
     }
