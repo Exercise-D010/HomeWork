5dc4483713fd4fd94cf73a140277858e03dd40eb
giang <iservice91@gmail.com>
2019-10-03 19:41:10 +0700

    create brand profile

---
+++ src/pages/profile/profile.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './profile.scss';
+
+class ProfilePage extends React.Component{
+  render(){
+    return(
+      <React.Fragment>
+
+      </React.Fragment>
+    )
+  }
+}
+
+export default ProfilePage
