19c98787fbe5a3e0f568e913fa100cc08f0c75ee
giang <iservice91@gmail.com>
2019-10-03 19:23:57 +0700

    create brand home

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import './home.scss';
 
-
 class HomePage extends React.Component{
-    render(){
-        return(
-            <React.Fragment>
-                
-            </React.Fragment>
-        )
-    }
+  render(){
+    return(
+      <React.Fragment>
+
+      </React.Fragment>
+    )
+  }
 }
 
 export default HomePage
