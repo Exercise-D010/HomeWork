f436e403d4aa2e968572d65094ddecb3440ec785
giang <iservice91@gmail.com>
2019-10-03 19:31:14 +0700

    create brand login

---
+++ src/pages/login/login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './login.scss';
+
+class LoginPage extends React.Component{
+  render(){
+    return(
+      <React.Fragment>
+
+      </React.Fragment>
+    )
+  }
+}
+
+export default LoginPage
